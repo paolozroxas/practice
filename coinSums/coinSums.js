5634228dd80d9d90ffd92cda935b5988a40a9d90
Paolo Z Roxas <paolo.z.roxas@gmail.com>
2018-02-07 09:56:51 -0800

    incomplete...

--- coinSums/coinSums.js
+++ coinSums/coinSums.js
@@ -24,8 +24,37 @@ makeChange(1) === 1
 makeChange(2) === 2
 */
 
+var coinValues = [200,100,50,20,10,5,2,1];
 var makeChange = function(total) {
+	var count = {count: 0};
+	makeChangeHelper(total, total, coinValues, count);
+	return count.count;
 
 };
+var makeChangeHelper = function(total, val, coins, count) {
+	if (coins.length === 1) {
+		count.count++;
+	} else {
+		for (var i  = 0; i < coins.length; i++) {
+			var coin = coins[i];
+			var times = 0;
+			while (coin*times <= val) {
+				times++
+			} //now you have times.
+			if (coin*times > val) {
+				break;
+			}
+			while(times >= 0) {
+				if (coin*times === val) {
+					count.count++;
+				} else {
+					makeChangeHelper(total, val - coin*times, coins.slice(1), count);
+				}
+				times--;
+			}
+			
+		}
+	}
 
+};
 
