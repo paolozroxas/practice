e77f975c3bcc339c0320104e792cc553bda9b5b5
Paolo Z Roxas <paolo.z.roxas@gmail.com>
2018-02-14 10:00:58 -0800

    all anagrams generating too many combs

--- allAnagrams/allAnagrams.js
+++ allAnagrams/allAnagrams.js
@@ -13,5 +13,24 @@
   */
 
 var allAnagrams = function(string) {
-  // Your code here.
+    var fullString = string;
+    var fullStringArr = fullString.split('');
+
+    var prepender = function(letters, combs) {
+        var arr = [];
+        for (var i = 0; i < combs.length; i++) {
+            for (var j = 0; j < letters.length; j++) {
+                arr.push(letters[j]+combs[i]);
+            }
+        }
+        return arr;
+    }
+    
+    var ans = fullStringArr;
+    for (var i = 0; i < string.length - 1; i++) {
+        ans = prepender(fullStringArr, ans);
+    }
+    return ans;
+
 };
+
