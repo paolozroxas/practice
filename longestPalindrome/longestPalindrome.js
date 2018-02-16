811bd9b73d9d16a7b6da7f855574cbd54567cb19
Paolo Z Roxas <paolo.z.roxas@gmail.com>
2018-02-16 09:37:59 -0800

    longest palindrome complete

--- longestPalindrome/longestPalindrome.js
+++ longestPalindrome/longestPalindrome.js
@@ -1,10 +1,38 @@
-/**
-* Implement a function that finds the longest palindrome in a given string.
-* For example, in the string "My dad is a racecar athlete", the longest
-* palindrome is "a racecar a". Count whitespaces as valid characters. Other
-* palindromes in the above string include "dad", "ete", " dad " (including
-* whitespace on each side of dad).
-*/
-
 var longestPalindrome = function (string) {
+  var palindromes = {};
+
+  //iterates through all substrings.
+  for (var i = 0; i < string.length; i++) {
+    for (var j = 1; j <= string.length; j++) {
+        if (j > i) {
+            //ask if substring is a palindrome. if yes,
+            //then add to palindromes object
+            var substr = string.slice(i, j);
+            if (isPalindrome(substr)) {
+                palindromes[substr] = true;
+            }
+        }
+    }
+  }//end of outer for loop
+  
+  var longest = '';
+  for (var pal in palindromes) {
+      if (pal.length > longest.length) {
+          longest = pal;
+      }
+  }
+
+  return longest;
+
 };
+
+//is the string a palindrome?
+var isPalindrome = function(string) {
+    for (var i = 0; i < string.length / 2; i++) {
+        var j = string.length - 1 - i;
+        if (string[i] !== string[j]) {
+            return false;
+        }
+    }
+    return true;
+}
