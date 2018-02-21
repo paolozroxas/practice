6ab07a83b1d1b9c071c03f3183f5846a24a553a1
Paolo Z Roxas <paolo.z.roxas@gmail.com>
2018-02-21 09:57:05 -0800

    spiral traversal incomplete

--- spiralTraversal/spiralTraversal.js
+++ spiralTraversal/spiralTraversal.js
@@ -15,6 +15,87 @@
  */
 
 var spiralTraversal = function(matrix) {
+  var xlen = matrix.length;
+  var ylen = matrix[0].length;
 
-  // TODO: Implement me!
+  var top = 0;
+  var left = 0;
+  var right = ylen - 1;
+  var bottom = xlen - 1;
+  var count = [0];
+
+  var spiral = [];
+
+  while (1) {
+    
+    if (traverseTop(matrix, top, left, right - 1, spiral, count, xlen * ylen)) {
+        return spiral;
+    }
+
+    if (traverseRight(matrix, right, top, bottom - 1, spiral, count, xlen * ylen)) {
+        return spiral;
+    }
+
+    if (traverseBottom(matrix, bottom, right, left + 1, spiral, count, xlen * ylen)) {
+        return spiral;
+    }
+
+    if (traverseLeft(matrix, left, bottom, top + 1, spiral, count, xlen * ylen)) {
+        return spiral;
+    }
+
+    top++;
+    right--;
+    bottom--;
+    left++;
+
+  }
 };
+
+var traverseTop = function(matrix, topIndex, start, end, outputArray, count, max) {
+    for (var i = start; i <= end; i++) {
+        outputArray.push(matrix[topIndex][i]);
+        console.log('spiral is now', outputArray);
+        count[0]++;
+        if (count[0] >= max) {
+            return true;
+        }
+    }
+    return false;
+}
+
+var traverseRight = function(matrix, rightIndex, start, end, outputArray, count, max) {
+    for (var i = start; i <= end; i++) {
+        outputArray.push(matrix[i][rightIndex]);
+        console.log('spiral is now', outputArray);
+        count[0]++;
+        if (count[0] >= max) {
+            return true;
+        }
+    }
+    return false;
+}
+
+var traverseBottom = function(matrix, bottomIndex, start, end, outputArray, count, max) {
+    for (var i = start; i >= end; i--) {
+        outputArray.push(matrix[bottomIndex][i]);
+        console.log('spiral is now', outputArray);
+        count[0]++;
+        if (count[0] >= max) {
+            return true;
+        }
+    }
+    return false;
+}
+
+var traverseLeft = function(matrix, leftIndex, start, end, outputArray, count, max) {
+    for (var i = start; i >= end; i--) {
+        outputArray.push(matrix[i][leftIndex]);
+        console.log('spiral is now', outputArray);
+        count[0]++;
+        if (count[0] >= max) {
+            return true;
+        }
+    }
+    return false;
+}
