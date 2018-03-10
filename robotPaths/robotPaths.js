7879a97f875c12290b2275f85393eb726d937310
Paolo Z Roxas <paolo.z.roxas@gmail.com>
2018-03-10 10:00:44 -0800

    incomplete robot paths

--- robotPaths/robotPaths.js
+++ robotPaths/robotPaths.js
@@ -25,9 +25,52 @@ var makeBoard = function(n) {
   board.hasBeenVisited = function(i, j) {
     return !!this[i][j];
   };
+  board.makeCopy = function() {
+    var newBoard = makeBoard(n);
+    for (var i = 0; i < n; i++) {
+      for(var j = 0; j < n; j++) {
+        newBoard[i][j] = this[i][j];
+      }
+    }
+    return newBoard;
+  };
   return board;
 };
 
 var robotPaths = function(n, board, i, j) {
+    debugger
+  if (i === n - 1 && j === n - 1) {
+    return 1;
+  }
+
+  board.togglePiece(i, j);
+
+  var validChoices = [];
+  if (i - 1 >= 0 && !board.hasBeenVisited(i - 1, j)) {
+    validChoices.push([i - 1, j]);
+  }
+  if (j + 1 < n && !board.hasBeenVisited(i, j + 1)) {
+    validChoices.push([i, j + 1]);
+  }
+  if (i + 1 < n && !board.hasBeenVisited(i + 1, j)) {
+    validChoices.push([i + 1, j]);
+  }
+  if (j - 1 >= 0 && !board.hasBeenVisited(i, j - 1)) {
+    validChoices.push([i, j - 1]);
+  }
+
+  if (validChoices.length === 0) {
+    return 0;
+  }
+
+  var validPaths = validChoices.reduce(function(x, y) { // each element is [i, j]
+    return robotPaths(n, board.makeCopy(), x[0], x[1]) + robotPaths(n, board.makeCopy(), y[0], y[1]);
+  });
+
+  return validPaths;
+
 };
 
+// //Tests:
+// var board = makeBoard(2);
+// console.log('number of paths:', robotPaths(2, board, 0, 0));
